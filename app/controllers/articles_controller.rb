06645bc188d6a36c729226365d59ddb55544e082
daniel130300 <daniel130300@outlook.com>
2021-09-19 23:33:33 -0600

    Final CRUD Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,61 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
+       
     end
 
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        
+    end
+
+    def create
+        @article = Article.new(article_params)
+        # render plain: @article.inspect
+        if @article.save
+            flash[:notice] = "Article was created successfully"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end 
+
+    def destroy
+        if @article.destroy
+            flash[:notice] = "Article was deleted successfully"
+        else
+            flash[:alert] = "There was an error deleting the article"
+        end
+
+        redirect_to articles_path
+    end
+
+    private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
