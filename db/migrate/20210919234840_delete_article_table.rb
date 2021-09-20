729183c2b445eee1c0b7d3bef423325933ee6c86
daniel130300 <daniel130300@outlook.com>
2021-09-19 19:55:28 -0600

    Article listing added

---
+++ db/migrate/20210919234840_delete_article_table.rb
@@ -0,0 +1,5 @@
+class DeleteArticleTable < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :articles
+  end
+end
---
+++ db/migrate/20210919235001_create_table_articles.rb
@@ -0,0 +1,9 @@
+class CreateTableArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.text :title
+      t.text :description
+    t.timestamps
+    end
+  end
+end
