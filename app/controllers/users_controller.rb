f1cd224c1f571ced638af492eac6bc939d96adfe
daniel130300 <daniel130300@outlook.com>
2021-09-21 16:56:09 -0600

    deploy v1.0.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,9 +43,9 @@ class UsersController < ApplicationController
 
     def destroy
         @user.destroy
-        session[:user_id] = nil
+        session[:user_id] = nil if @user == current_user
         flash[:notice] = "Account and all associated articles deleted"
-        redirect_to signup_path
+        redirect_to articles_path
     end
 
     private 
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
     end 
 
     def require_same_user 
-        if current_user != @user
+        if current_user != @user && !current_user.admin?
             flash[:alert] = "You can only edit or delete your own account"
             redirect_to user_path(current_user)
         end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_172526) do
+ActiveRecord::Schema.define(version: 2021_09_21_215332) do
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_09_20_172526) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
